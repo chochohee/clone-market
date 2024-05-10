84f2aa64c371fde9e4948295f93bf7c2ada54075
chochohee <168400614+chochohee@users.noreply.github.com>
2024-05-10 16:04:33 +0900

    🔧오류확인 및 강의과정 배포준비
    - 커밋하는 과정에서 파일이 초기화되서 지속 오류(버그)발생😢 시간관계상 추후 재수정예정
    - 강의상 델타스페이스 업로드 위한 백엔드 테이블제작 SQL문 추가

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,UploadFile,Form, Response
+from fastapi import FastAPI,UploadFile,Form,Response
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
@@ -8,19 +8,34 @@ import sqlite3
 con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
 
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items (
+               id INTEGER PRIMARY KEY,
+               title TEXT NOT NULL,
+               image BLOB,
+               price INTEGER NOT NULL,
+               description TEXT,
+               place TEXT NOT NULL,
+               insertAt INTEGER NOT NULL
+            );
+
+            """)
+
 app = FastAPI()
 
 @app.post('/items')
 async def create_item(image:UploadFile,
                 title:Annotated[str,Form()],
-                price: Annotated[int,Form()],
+                price:Annotated[int,Form()],
                 description:Annotated[str,Form()],
                 place:Annotated[str,Form()],
-                insertAt:Annotated[int,Form()]):
+                insertAt:Annotated[int,Form()]
+                ):
+   
    image_bytes = await image.read()
    cur.execute(f"""
-               INSERT INTO items (title,image,price,description,place)
-               VALUES ('{title}','{image_bytes.hex()}','{price}','{description}','{place}',{insertAt})
+               INSERT INTO items (title,image,price,description,place,insertAt)
+               VALUES ('{title}','{image_bytes.hex()}',{price}','{description}','{place}',{insertAt})
                """)
    con.commit()
    return '200'
