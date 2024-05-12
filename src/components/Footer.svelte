751197298b72c5164d601bfb2d85fbfdcc21e453
chochohee <168400614+chochohee@users.noreply.github.com>
2024-05-13 02:14:09 +0900

    🔹글쓰기 리스트업 구현
    - firebase 연동을 통한 database구축
    - 글쓰기 기능 이용시 database에 저장된 title, price, place, insertAt(time) 홈화면(main)에 리스트업
    - 글쓰기로 이미지 업로드시 storage에 저장하여 database에 같이 저장 후 불러와 홈화면(main)에 리스트업
    +Home버튼 클릭시 홈화면 이동, 글쓰기 submit 완료시 홈화면 이동

---
+++ src/components/Footer.svelte
@@ -0,0 +1,52 @@
+<script>
+    export let location;
+
+    const moveToHome = () =>{
+        window.location.hash = '/'
+    }
+</script>
+
+<footer>
+    <div class="footer-block" >
+      <button class="footer-icons" on:click={moveToHome}>
+        <div class="footer-icons__img" id="home-btn" >
+          {#if location==="home"}
+            <img src="assets/home.svg" alt=""/>
+            {:else}
+            <img src="assets/white_home.svg" alt="">
+          {/if}
+        </div>
+        <div class="footer-icons__desc">홈</div>
+      </button>
+      <div class="footer-icons">
+        <div class="footer-icons__img">
+          <img src="assets/doc.svg" alt="" />
+        </div>
+        <div class="footer-icons__desc">동네생활</div>
+      </div>
+      <div class="footer-icons">
+        <div class="footer-icons__img">
+          <img src="assets/rocation.svg" alt="" />
+        </div>
+        <div class="footer-icons__desc">내 근처</div>
+      </div>
+      <div class="footer-icons">
+        <div class="footer-icons__img">
+          <img src="assets/chat.svg" alt="" />
+        </div>
+        <div class="footer-icons__desc" id="chat">채팅</div>
+      </div>
+      <div class="footer-icons">
+        <div class="footer-icons__img">
+          <img src="assets/user.svg" alt="" />
+        </div>
+        <div class="footer-icons__desc">나의 당근</div>
+      </div>
+    </div>
+  </footer>
+
+  <style>
+    #home-btn{
+      width: 35px;
+    }
+  </style>
--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,25 +1,89 @@
-<form id="write-form">
+<script>
+import { getDatabase, push, ref, set } from "firebase/database";
+import Footer from "../components/Footer.svelte";
+import { getStorage, ref as refImage, uploadBytes, getDownloadURL} from "firebase/storage";
+
+let title;
+let price;
+let description;
+let place;
+let files;
+
+const storage = getStorage();
+const db = getDatabase();
+
+async function writeUserData(imgUrl) {
+  push(ref(db, "items/"), {
+    title,
+    place,
+    description,
+    price,
+    insertAt:new Date().getTime(),
+    imgUrl,
+  });
+  alert("게시글이 작성되었습니다.")
+  window.location.hash = "/";
+}
+
+// // 'file' comes from the Blob or File API
+// uploadBytes(storageRef, file).then((snapshot) => {
+//   console.log('Uploaded a blob or file!');
+// });
+
+
+$: if(files) console.log(files[0]);
+
+const uploadFile = async() =>{
+  const file = files[0];
+  const name = file.name;
+  const imgRef = refImage(storage, name);
+  await uploadBytes(imgRef, file);
+  const url = await getDownloadURL(imgRef)
+  return url;
+};
+
+const handleSubmit = async() => {
+  const url = await uploadFile();
+  writeUserData(url);
+}
+// on:click={uploadFile}
+</script>
+<form id="write-form" on:submit|preventDefault={handleSubmit}>
     <div>
       <label for="image">이미지</label>
-      <input type="file" id="image" name="image" />
+      <input type="file" bind:files id="image" name="image" />
     </div>
     <div>
       <label for="title">제목</label>
-      <input type="text" id="title" name="title" />
+      <input type="text" id="title" name="title" bind:value={title} />
     </div>
     <div>
       <label for="price">가격</label>
-      <input type="number" id="price" name="price" />
+      <input type="number" id="price" name="price" bind:value={price} />
     </div>
     <div>
       <label for="description">설명</label>
-      <input type="text" id="description" name="description" />
+      <input type="text" id="description" name="description" bind:value={description} />
     </div>
     <div>
       <label for="place">장소</label>
-      <input type="text" id="place" name="place" />
+      <input type="text" id="place" name="place" bind:value={place} />
     </div>
     <div>
-      <button type="submit">글쓰기</button>
+      <button class="write-button" type="submit">글쓰기</button>
     </div>
   </form>
+
+  <Footer location="write"/>
+
+  <style>
+    .write-button{
+      background-color: tomato;
+      margin-left: 30px;
+      margin-top: 10px;
+      padding: 5px 5px 5px 5px;
+      font-size: large;
+      color: white;
+      border-radius: 10px;
+    }
+  </style>
